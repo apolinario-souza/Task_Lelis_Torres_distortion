d36ea4221e1dbc95d3336e243d44293d81d86c9e
TÉRCIO APOLINÁRIO DE SOUZA <40078879+edftercio@users.noreply.github.com>
2022-02-05 16:24:26 -0300

    Add files via upload

---
+++ contents/constants.py
@@ -0,0 +1,54 @@
+import pygame
+
+#Configuration trials
+N_TRIALS = 24
+
+#Configuration screen
+WIDTH, HEIGHT = 1366,768
+
+#Configuration of position target
+
+TARGET_x_1 = 1.25
+TARGET_y_1 = .10
+
+TARGET_x_2 = 1
+TARGET_y_2 = .10  
+
+TARGET_x_3 = .8
+TARGET_y_3 = .10  
+
+TARGET_x_4 = 1.25
+TARGET_y_4 = 1.8 
+
+TARGET_x_5 = 1
+TARGET_y_5 = 1.8 
+
+TARGET_x_6 = 0.8
+TARGET_y_6 = 1.8    
+ 
+
+
+
+
+#Configuration Home and Targets 
+DIAMETER_H = 60
+DIAMETER_T = 20
+
+### Times
+HOLDING = 1000
+WARNING = 2000
+EXECUTION = 1500
+RANDOM_INTERVAL = (1000, 1500)
+TIME_IMG = 3000 
+
+
+
+# Color rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLACK_2 = (15, 15, 15)
+BLUE = (0, 0, 255)
+GREY = (128,128,128, 128)
+GREEN = (105, 250, 0)
+
